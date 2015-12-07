e9d7f05150707face05c134da1c7e4b8d4c1de44
Michael Narciso <narkeeso@gmail.com>
2015-12-06 22:47:59 -0800

    Establish initial router folder structure

--- api/src/app/server.js
+++ api/src/app/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import falcor from 'falcor';
 import FalcorRouter from 'falcor-router';
 import FalcorServer from 'falcor-express';
-import {Observable} from 'rx';
 
 import hsClient from '../lib/hs-api-client';
+import CardsByClass from './routers/cards-by-class';
 
 const app = express();
 
@@ -22,64 +21,6 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-class CardsRouter extends FalcorRouter.createClass([
-  {
-    route: 'cardsByClass[{keys:playerClass}].length',
-    get: (pathSet) => {
-      const playerClass = pathSet.playerClass[0];
-
-      return Observable.fromPromise(
-        hsClient('GET', `/cards/classes/${playerClass}?collectible=1`)
-      )
-      .map((collectibles) => {
-        const cards = collectibles.filter((collectible) => {
-          return collectible.type !== 'Hero';
-        });
-
-        return {
-          path: ['cardsByClass', playerClass, 'length'],
-          value: cards.length
-        };
-      });
-    }
-  },
-  {
-    route: 'cardsByClass[{keys:playerClass}][{ranges:index}]["name", "img", "imgGold"]',
-    get: (pathSet) => {
-      const playerClass = pathSet.playerClass[0];
-      const range = pathSet.index[0];
-
-      return Observable.fromPromise(
-        hsClient('GET', `/cards/classes/${playerClass}?collectible=1`)
-      )
-      .flatMap((collectibles) => {
-        return Observable
-          .fromArray(collectibles)
-          .filter((collectible) => {
-            return collectible.type !== 'Hero';
-          });
-      })
-      .filter((card, cardIndex) => {
-        return cardIndex >= range.from && cardIndex <= range.to;
-      })
-      .flatMap((card, cardIndex) => {
-        return Observable
-          .fromArray(pathSet[3])
-          .map((key) => {
-            return {
-              path: ['cardsByClass', playerClass, cardIndex + range.from, key],
-              value: card[key]
-            };
-          });
-      });
-    }
-  }
-]) {
-  constructor() {
-    super();
-  }
-}
-
 app.use('/model.json', FalcorServer.dataSourceRoute((req, res, next) => {
-  return new CardsRouter;
+  return new CardsByClass;
 }));
