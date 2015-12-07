09064b9f352fb225c3a73eef13af6450b91331d9
Michael Narciso <narkeeso@gmail.com>
2015-12-06 23:07:13 -0800

    Adds example json result in cards by class router

--- api/src/app/routers/cards-by-class.js
+++ api/src/app/routers/cards-by-class.js
@@ -1,6 +1,30 @@
 import FalcorRouter from 'falcor-router';
 import {Observable} from 'rx';
 
+/**
+ * Assuming the query/path set is:
+ * [
+ *  ['cardsByClass', 'Hunter', '{from: 0, 1}', ["name", "img"]],
+ *  ['cardsByClass', 'Hunter', 'length']
+ * ]
+ *
+ * The resulting json from this router based on above path sets:
+ * {
+ *  cardsByClass: {
+ *    Hunter: {
+ *      0: {
+ *        name: 'Hunter's Mark',
+ *        img: 'http://wow.zamimg.com/images/hearthstone/cards/enus/original/CS2_084.png'
+ *      },
+ *      1: {
+ *        name: 'Arcane Shot',
+ *        img: 'http://wow.zamimg.com/images/hearthstone/cards/enus/original/DS1_185.png'
+ *      },
+ *      length: 49
+ *    }
+ *  }
+ * }
+ */
 class CardsByClass extends FalcorRouter.createClass([
   {
     route: 'cardsByClass[{keys:playerClass}].length',
